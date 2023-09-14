56cf4c135d6166de3f872c9a6237921e59b3b5ef
Lyra Freund <lyrafreund@gmail.com>
2023-09-13 18:25:14 -0600

    complete TaskLister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    buildToDo(e.target["new-task-description"].value)
+    
+    }
+   )
+
+   function buildToDo(todo){
+    console.log(todo)
+    let p = document.createElement("p")
+    let btn = document.createElement("button")
+    btn.addEventListener("click",deleteBtn)
+    p.textContent = `${todo}  `
+    btn.textContent = "x"
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector("#tasks").appendChild(p)
+    
+   }
+
+   function deleteBtn(e){
+    e.target.parentNode.remove()
+   }
+
 });
